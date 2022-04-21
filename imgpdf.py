e4e8863c57ba452466abdd7d7b8553837a9ba28f
chocolate_flavorz <longhorn1102@gmail.com>
2022-04-21 13:06:50 +0900

    huge update for imgpdf

--- imgpdf.py
+++ imgpdf.py
@@ -1,32 +1,28 @@
+# imgpdf ver2
+
 from PIL import Image
-from fpdf import FPDF
 import sys
 import os
 import glob
 
-SIZE = (210, 297)
-OUTPUT = 'imgpdf.pdf'
-margin = 0
+# A4 72dpi
+SIZE = (595, 842)
+# a4 300dpi
+SIZE_HDPI = (2480, 3508)
+# output filename
+OUTPUT = "imgpdf.pdf"
 
 arg_length = len(sys.argv)
 arg_list = sys.argv[1:arg_length]
+img_list = []
 
-if arg_list[0] == 'all':
-    arg_list = glob.glob('*.jpg', recursive = True)
+if arg_list[0] == "all":
+    arg_list = glob.glob("*.jpg", recursive=True)
 
-image = Image.open(arg_list[0])
-width, height = image.size
-pdf = FPDF(unit="pt", format=[width + 2 * margin, height + 2 * margin])
-pdf.add_page()
-pdf.image(arg_list[0], margin, margin)
-image.close()
+for arg in arg_list:
+    img = Image.open(arg)
+    img = img.rotate(270, expand=True)
+    img = img.resize(SIZE_HDPI)
+    img_list.append(img)
 
-arg_list = arg_list[1:]
-
-for image_path in arg_list:
-    image = Image.open(image_path)
-    pdf.add_page()
-    pdf.image(image_path, margin, margin)
-    image.close()
-
-pdf.output(OUTPUT, "F")
+img_list[0].save(OUTPUT, save_all=True, append_images=img_list[1:], resolution=300)
