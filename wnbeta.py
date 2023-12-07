86230f552e88de7d95d805710a20bf174fdfc0d5
chocolate_flavorz <longhorn1102@gmail.com>
2023-12-07 15:52:13 +0900

    fitting

--- wnbeta.py
+++ wnbeta.py
@@ -2,6 +2,7 @@ import numpy
 import csv
 import sys
 import glob
+import scipy.optimize as optimize
 from math import log10
 
 PI = numpy.pi
@@ -44,19 +45,41 @@ with open(input_file, 'r') as f:
         row = trim(row)
         matrix1.append(row)
 
+# ugly cat, but it works
 r = []
+fp = []
+thetaP2 = []
 for i in range(len(matrix1)):
     r_row = []
     r_row.append(float(matrix1[i][0]))
+    fp.append(float(matrix1[i][0]))
     rb = (2 * PI * convf(float(matrix1[i][0])) / float(matrix1[i][1]))
-    rb_m1 = rb - (PI / P)
+    rb_m1 = (rb - (PI / P)) * P
     r_row.append(rb_m1)
-    ib = rb * float(matrix1[i][2]) / -(40 * PI * log10(E))
+    ib = (rb * float(matrix1[i][2]) / -(40 * PI * log10(E))) * P
     r_row.append(ib)
-    r_row.append(rb_m1 * rb_m1 - ib * ib)
+    r_row.append((rb_m1 * rb_m1 - (ib * ib)) )
+    thetaP2.append((rb_m1 * rb_m1 - (ib * ib)) )
     r.append(r_row)
 
 with open(output_file, 'w', newline='') as f:
     evilwriter = csv.writer(f, delimiter=' ')
     for c in r:
         evilwriter.writerow(c)
+
+#r = [fp, reB - (pi/P), imB, thetapP^2]
+def f(x, a, b, c):
+    return c * c * (x - b) * (x - a)
+print ("fp47 = ", convf(.47))
+print("initial guess: a, b, c = .47 .48 13")
+popt, pcov = optimize.curve_fit(f, fp, thetaP2, [.47, .48, 13])
+print("a, b, c =", popt)
+print("err =", numpy.sqrt(numpy.diag(pcov)))
+
+a = popt[0]
+b = popt[1]
+c = popt[2]
+theta_u = c * (a/2 - b/2) 
+kappa = c* abs(a-b) / 2
+print("theta_u*P =", theta_u, "kappa*P =", kappa)
+print("theta_u kappa =", theta_u/P,kappa/P)
