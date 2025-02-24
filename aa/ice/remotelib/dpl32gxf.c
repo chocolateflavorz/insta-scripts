3f85705b1b4c4fb6e085886b51429b71dfe636eb
chocolate_flavorz <longhorn1102@gmail.com>
2025-02-24 17:50:35 +0900

    Delete aa directory

--- aa/ice/remotelib/dpl32gxf.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <stdbool.h>
-
-#include "remote.h"
-#include "dsg800.h"
--- aa/run.sh
+++
@@ -1,8 +0,0 @@
- #!/bin/bash
-
-gcc -Og -Wall -g3 --std=c2x -fsanitize=address,undefined \
-  ./src/main.c \
-  ./src/con.c \
-  ./src/tinyexpr.c \
-  -lm \
-  -o invader
